5aab4f5c0ed1e0d19090f8c6bf9ba8cf4ff7d6bd
cedrickjames <cedrickorozo@gmail.com>
2023-04-26 15:13:20 +0800

    ok

--- login.php
+++ login.php
@@ -189,11 +189,19 @@ if(isset($_POST['submit'])){
         </form>
 
       </div>
+      <footer class="w-full bg-white m-4">
+
+<!-- <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> -->
+<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> 2023 <a href="https://flowbite.com/" class="hover:underline">MIS</a>. All Rights Reserved.</span>
+</div>
+</footer>
     </div>
+     
+
   </div>
+  
 </section>
 
- 
 
 
  
